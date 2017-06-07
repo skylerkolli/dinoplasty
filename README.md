539e145579797b77c587c5355c6d7f1bc0aef1c0
skylerkolli <skylerkolli@gmail.com>
2017-06-07 16:32:25 -0400

    add

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dinoplasty
 
-Dinosaurs!
+## Day 3 Homework
+
+*store the dinosaurs in an array as well as in the DOM
+
+### Mega Bonus Credit
+*Add a promote/fav button, jsut like you did yesterday
+*Add a delete buttonm just like yesterday
--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const app = {
-    init(formSelector) {
-        this.max = 0
-        document
-            .querySelector(formSelector)
-            .addEventListener("submit",this.addDino.bind(this) )
-    },
-    
-    addDino(ev) {
-        ev.preventDefault()
-        
-        const dino = {
-            id: this.max + 1,
-            name:ev.target.dinoName.value,
-        }
-        console.log(dino.Name, dino.id)
-        ++ this.max
-    },
+  init(selectors) {
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addDino.bind(this))
+  },
+
+  addDino(ev) {
+    ev.preventDefault()
+
+    const dino = {
+      id: this.max + 1,
+      name: ev.target.dinoName.value,
+    }
+
+    const listItem = this.renderListItem(dino)
+    this.list.appendChild(listItem)
+
+    ++ this.max
+  },
+
+  renderListItem(dino) {
+    const item = document.createElement('li')
+    item.textContent = dino.name
+    return item
+  }
 }
 
-app.init("#dino-form")
+app.init({
+  formSelector: '#dino-form',
+  listSelector: '#dino-list',
+})
