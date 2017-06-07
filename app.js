986ec799ebfed9e2d0ef9bc533e1886141c3f54d
skylerkolli <skylerkolli@gmail.com>
2017-06-07 16:16:01 -0400

    adding dino object

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const app = {
     init(formSelector) {
+        this.max = 0
         document
             .querySelector(formSelector)
-            .addEventListener("submit",this.addDino )
+            .addEventListener("submit",this.addDino.bind(this) )
     },
     
     addDino(ev) {
         ev.preventDefault()
-        const dinoName = ev.target.dinoName.value
-        console.log("submitted!")
+        
+        const dino = {
+            id: this.max + 1,
+            name:ev.target.dinoName.value,
+        }
+        console.log(dino.Name, dino.id)
+        ++ this.max
     },
 }
 
